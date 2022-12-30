b43248708e5615aa528d43df81c12957c46735cb
Florence Leclercq <leclercqflorence122@orange.fr>
2022-12-30 18:55:20 +0100

    Premier commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Jeu "Devinez le bon nombre"
+
+- Petit projet réalisé dans le cadre de ma formation "Développeur d'application multimédia" avec Doranco.
+- HTML5, CSS3 et javaScript.
---
+++ script.js
@@ -0,0 +1,64 @@
+var number= document.querySelector(".number");
+var check= document.querySelector(".fa-circle-check");
+var answer= document.querySelector(".answer");
+var h2= document.querySelector("h2");
+var reset= document.querySelector(".fa-sharp");
+var robot = getRandomNumber();
+var lower = 1;
+var higher = 100;
+
+function getRandomNumber() {
+    return(Math.floor(Math.random() * 100)+1);
+}
+
+check.addEventListener('click', function() {
+    console.log("nombre du robot : ",robot);
+    numberChosen = number.value;
+
+    if (answer.innerHTML== 'Gagné !') {
+        newGame()
+        return
+    }
+
+    if (numberChosen < 1 || numberChosen > 100) {
+        answer.innerHTML= "Veuillez saisir un nombre entre 1 et 100"; 
+        answer.style.color= "red";
+        return false
+    }
+
+    if (isNaN(numberChosen)) {
+        answer.innerHTML= 'Oups ! Ce n\'est pas un nombre...'
+        answer.style.color= 'red'
+        return 'Not a Number!';
+    }
+
+    if(numberChosen != robot  ) {
+        answer.innerHTML= "Perdu..."; 
+        answer.style.color= "red";
+        if (numberChosen > robot) {
+            h2.innerHTML="Veuillez saisir un nombre entre " + lower + " et " + numberChosen ;
+            higher=numberChosen;
+            console.log("higher : ", higher);
+        } else if (numberChosen < robot) {
+            h2.innerHTML="Veuillez saisir un nombre entre " + numberChosen + " et " + higher;
+            lower=numberChosen;
+            console.log("lower : ", lower);
+        }
+    } else if (numberChosen == robot ){
+        answer.innerHTML= 'Gagné !'
+        answer.style.color= "green";
+        answer.style.fontWeight= "bold";
+    }
+})
+
+function newGame() {
+    h2.innerHTML="Veuillez saisir un nombre entre 1 et 100";
+    number.value="";
+    answer.innerHTML="Perdu ou gagné ?";
+    answer.style.color= "gray";
+    answer.style.fontWeight= "normal";
+    robot = getRandomNumber();
+}
+
+reset.addEventListener('click', newGame)
+
